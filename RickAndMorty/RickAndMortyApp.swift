dd172dbc13f4a62e7cf501530dccf5b9fa060fd2
ivr88 <ivr88@yandex.ru>
2024-10-12 19:10:46 +0300

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Вадим Исламов on 12.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
