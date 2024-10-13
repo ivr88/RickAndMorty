3c1030f97c2c701d9da7463820f8bc38163f428c
ivr88 <ivr88@yandex.ru>
2024-10-13 22:27:55 +0300

    Update ContentView.swift
    Обновил ContentView, добавил таблицу с отображением основных данных

--- RickAndMorty/View/ContentView.swift
+++ RickAndMorty/View/ContentView.swift
@@ -1,14 +1,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var listViewModel = ListViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List (listViewModel.characters, id: \.id) { character in
+            HStack {
+                AsyncImage(url: URL(string: character.image)) { image in
+                    image.image?
+                        .resizable()
+                        .frame(width: 84, height: 64)
+                        .clipShape(.rect(cornerRadius: 10))
+                }
+                    
+                VStack (alignment: .leading) {
+                    Text (character.name)
+                    HStack {
+                        Text(character.status.rawValue)
+                        Text("•")
+                        Text(character.species.rawValue)
+                    }
+                    Text(character.gender.rawValue)
+                }
+            }
         }
-        .padding()
+        .onAppear {
+            listViewModel.fetchCharacters()
+        }
     }
 }
 
