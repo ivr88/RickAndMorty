b79088f169e2abc075601a1d300c3ccde5f8ac08
ivr88 <ivr88@yandex.ru>
2024-10-15 22:44:46 +0300

    Update DetailView.swift
    Изменил цвет фона у View

--- RickAndMorty/View/DetailView.swift
+++ RickAndMorty/View/DetailView.swift
@@ -2,7 +2,11 @@ import SwiftUI
 
 struct DetailView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Color.black
+            .ignoresSafeArea()
+            .overlay (
+            Text("Hello!")
+        )
     }
 }
 
