fe56e63d51bab7b273c5f43512af906431be6eaf
ivr88 <ivr88@yandex.ru>
2024-10-15 22:29:03 +0300

    Update ContentView.swift
    Добавил навигацию NavigationStack и NavigationLink для перехода к DetailView

--- RickAndMorty/View/ContentView.swift
+++ RickAndMorty/View/ContentView.swift
@@ -5,33 +5,40 @@ struct ContentView: View {
     @ObservedObject var listViewModel = ListViewModel()
     
     var body: some View {
-        List (listViewModel.characters, id: \.id) { character in
-            HStack {
-                AsyncImage(url: URL(string: character.image)) { image in
-                    image.image?
-                        .resizable()
-                        .frame(width: 84, height: 64)
-                        .clipShape(.rect(cornerRadius: 10))
-                }
-                    
-                VStack (alignment: .leading, spacing: 6) {
-                    Text (character.name)
-                        .font(.custom("IBMPlexSans-Medium", size: 18))
+        NavigationStack {
+            List (listViewModel.characters, id: \.id) { character in
+                NavigationLink {
+                    DetailView()
+                        .toolbarRole(.editor)
+                } label: {
                     HStack {
-                        Text(character.status.rawValue)
-                            .foregroundStyle(Color(character.statusColor))
-                        Text("•")
-                        Text(character.species.rawValue)
+                        AsyncImage(url: URL(string: character.image)) { image in
+                            image.image?
+                                .resizable()
+                                .frame(width: 84, height: 64)
+                                .clipShape(.rect(cornerRadius: 10))
+                        }
+                        
+                        VStack (alignment: .leading, spacing: 6) {
+                            Text (character.name)
+                                .font(.custom("IBMPlexSans-Medium", size: 18))
+                            HStack {
+                                Text(character.status.rawValue)
+                                    .foregroundStyle(Color(character.statusColor))
+                                Text("•")
+                                Text(character.species.rawValue)
+                            }
+                            .font(.custom("IBMPlexSans-SemiBold", size: 12))
+                            
+                            Text(character.gender.rawValue)
+                                .font(.custom("IBMPlexSans-Regular", size: 12))
+                        }
                     }
-                    .font(.custom("IBMPlexSans-SemiBold", size: 12))
-                    
-                    Text(character.gender.rawValue)
-                        .font(.custom("IBMPlexSans-Regular", size: 12))
                 }
             }
-        }
-        .onAppear {
-            listViewModel.fetchCharacters()
+            .onAppear {
+                listViewModel.fetchCharacters()
+            }
         }
     }
 }
