07f8aa49c264dd2e41c458bc466f42b35a9c046f
ivr88 <ivr88@yandex.ru>
2024-10-22 21:37:16 +0300

    Create Color.swift
    Создал расширение Color со списком кастомных цветов

---
+++ RickAndMorty/Color/Color.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+extension Color {
+    static let customLightBlack = Color("#1E1E20")
+    static let customGray = Color("#6D7885")
+    static let customTurquoise = Color("#42B4CA")
+    static let customBlack = Color("#151517")
+    static let customGreen = Color("#198737")
+    static let customDarkGray = Color("#686874")
+    static let customRed = Color("#D62300")
+}
