669c05bba936aa652811088ff26617082da2660d
ivr88 <ivr88@yandex.ru>
2024-12-21 15:34:33 +0300

    Update RickAndMortyApp.swift
    Добавил в RickAndMortyApp отслеживание интернет-соединения через networkManager

--- RickAndMorty/App/RickAndMortyApp.swift
+++ RickAndMorty/App/RickAndMortyApp.swift
@@ -2,9 +2,12 @@ import SwiftUI
 
 @main
 struct RickAndMortyApp: App {
+    @StateObject private var networkManager = NetworkMonitor()
+    
     var body: some Scene {
         WindowGroup {
             CoordinatorView()
+                .environmentObject(networkManager)
         }
     }
 }
