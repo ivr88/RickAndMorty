2263318c82659d1491493b85573d0377dec14dda
ivr88 <ivr88@yandex.ru>
2024-12-21 15:35:38 +0300

    Update CoordinatorView.swift
    Обновлены условия работы для CoordinatorView

--- RickAndMorty/View/CoordinatorView.swift
+++ RickAndMorty/View/CoordinatorView.swift
@@ -2,11 +2,15 @@ import SwiftUI
 
 struct CoordinatorView: View {
     
+    @ObservedObject private var networkMonitor = NetworkMonitor()
+    
     @State private var isActive = false
     
     var body: some View {
-        if isActive {
+        if networkMonitor.isConnected && isActive {
             ListView()
+        } else if !networkMonitor.isConnected {
+            ErrorView()
         } else {
             SplashView(isActive: $isActive)
         }
